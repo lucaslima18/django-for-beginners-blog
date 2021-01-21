6488beccec3ae48b63f60450bafa816580927dc3
lucaslima18 <lucas.ala1999@gmail.com>
2021-01-21 15:48:13 -0300

    server configs

--- blog_project/settings.py
+++ blog_project/settings.py
@@ -13,7 +13,7 @@ SECRET_KEY = 'l(a)h1y-!0#y&n_xsb^(4)rp-#!130ofvu#nqq*uff$0@2fg^@'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
